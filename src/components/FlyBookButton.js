d15b3c19ceea7ada57645a34586d3eb614fc5d49
Austin Jacobs <aust.t.jac@gmail.com>
2023-01-18 12:01:03 -0700

    Reworked TVL header. Added FlyBook Button

---
+++ src/components/FlyBookButton.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function FlyBookButton() {
+  return (
+    <button className='flybook-book-now-button fb-widget-type-frontend fb-default-category-id-0 fb-account-id-64 fb-entity-config-id- fb-domain-go.theflybook.com fb-protocol-https'>
+      BOOK NOW
+    </button>
+  );
+}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,35 @@
-import { Navbar, Link, Text, Dropdown, } from '@nextui-org/react';
+import { Navbar, Link, Dropdown, Image, Text, Button } from '@nextui-org/react';
 import { Layout } from '../components/styles/Layout';
-import { AcmeLogo } from '../assets/AcmeLogo';
 import { icons } from '../assets/Icons';
+import Logo from '../assets/cropped-tetonvalleylodge.png';
+import { SlPhone } from 'react-icons/sl';
 
 export default function App() {
   const collapseItems = ['Customers', 'Pricing', 'Company'];
 
   return (
     <Layout>
-      <Navbar isCompact variant='sticky'>
+      <Navbar variant='sticky' maxWidth='fluid' disableBlur>
         <Navbar.Toggle showIn='xs' />
         <Navbar.Brand
           css={{
             '@xs': {
-              w: '12%',
+              w: '22%',
+            },
+            '@sm': {
+              w: '14%',
+            },
+            '@md': {
+              w: '14%',
+            },
+            '@lg': {
+              w: '14%',
+            },
+            '@xl': {
+              w: '8%',
             },
           }}>
-          <AcmeLogo />
-          <Text b color='red' hideIn='xs'>
-            ACME
-          </Text>
+          <Image src={Logo} alt='TVL Fish Logo' />
         </Navbar.Brand>
         <Navbar.Content
           enableCursorHighlight
@@ -35,94 +45,126 @@ export default function App() {
                   px: 0,
                   dflex: 'center',
                   svg: { pe: 'none' },
-                  color: 'red',
+                  fontFamily: 'Roboto Condensed',
                 }}
                 iconRight={icons.chevron}
                 ripple={false}>
-                Features
+                <Text css={{ fontWeight: 700, color: '#506A63' }}>
+                  BOOK YOUR TRIP
+                </Text>
               </Dropdown.Button>
             </Navbar.Item>
             <Dropdown.Menu
-              aria-label='ACME features'
+              aria-label='Rivers'
               css={{
-                $$dropdownMenuWidth: '340px',
-                $$dropdownItemHeight: '70px',
+                marginRight: '15px',
+                $$dropdownMenuWidth: '400px',
+                $$dropdownItemHeight: '60px',
                 '& .nextui-dropdown-item': {
                   py: '$4',
-                  // dropdown item left icon
-                  svg: {
-                    color: '$secondary',
-                    mr: '$4',
-                  },
-                  // dropdown item title
                   '& .nextui-dropdown-item-content': {
                     w: '100%',
                     fontWeight: '$semibold',
+                    color: '#F75A0D',
                   },
                 },
               }}>
               <Dropdown.Item
-                key='autoscaling'
+                key='contact_us'
                 showFullDescription
-                description='ACME scales apps to meet user demand, automagically, based on load.'
-                icon={icons.scale}>
-                Autoscaling
+                description='Have a question about fishing or just want to say hi? We’d love to hear from you. Drop us a line and we will respond as soon as possible.'>
+                Contact Us
               </Dropdown.Item>
               <Dropdown.Item
-                key='usage_metrics'
+                key='trip_details'
                 showFullDescription
-                description='Real-time metrics to debug issues. Slow query added? We’ll show you exactly where.'
-                icon={icons.activity}>
-                Usage Metrics
-              </Dropdown.Item>
-              <Dropdown.Item
-                key='production_ready'
-                showFullDescription
-                description='ACME runs on ACME, join us and others serving requests at web scale.'
-                icon={icons.flash}>
-                Production Ready
-              </Dropdown.Item>
-              <Dropdown.Item
-                key='99_uptime'
-                showFullDescription
-                description='Applications stay on the grid with high availability and high uptime guarantees.'
-                icon={icons.server}>
-                +99% Uptime
-              </Dropdown.Item>
-              <Dropdown.Item
-                key='supreme_support'
-                showFullDescription
-                description='Overcome any challenge with a supporting team ready to respond.'
-                icon={icons.user}>
-                +Supreme Support
+                description="Ready to book the trip of a lifetime? Let's get started!">
+                Trip Details
               </Dropdown.Item>
             </Dropdown.Menu>
           </Dropdown>
-          <Navbar.Link css={{ color: 'red' }} href='#'>
-            Customers
-          </Navbar.Link>
-          <Navbar.Link css={{ color: 'red' }} href='#'>
-            Pricing
-          </Navbar.Link>
-          <Navbar.Link css={{ color: 'red' }} href='#'>
-            Company
+
+          <Dropdown isBordered>
+            <Navbar.Item>
+              <Dropdown.Button
+                auto
+                light
+                css={{
+                  px: 0,
+                  dflex: 'center',
+                  svg: { pe: 'none' },
+                  fontFamily: 'Roboto Condensed',
+                }}
+                iconRight={icons.chevron}
+                ripple={false}>
+                <Text css={{ fontWeight: 700, color: '#506A63' }}>RIVERS</Text>
+              </Dropdown.Button>
+            </Navbar.Item>
+            <Dropdown.Menu
+              aria-label='Rivers'
+              css={{
+                marginRight: '15px',
+                $$dropdownMenuWidth: '400px',
+                $$dropdownItemHeight: '60px',
+                '& .nextui-dropdown-item': {
+                  py: '$4',
+                  '& .nextui-dropdown-item-content': {
+                    w: '100%',
+                    fontWeight: '$semibold',
+                    color: '#F75A0D',
+                  },
+                },
+              }}>
+              <Dropdown.Item
+                key='teton_river'
+                showFullDescription
+                description='The Teton is more than just a river to us, it is part of everything we are.'>
+                Teton River
+              </Dropdown.Item>
+              <Dropdown.Item
+                key='south_fork'
+                showFullDescription
+                description='If you could only fish one river the rest of your life, the South Fork of the Snake would be the river to select.'>
+                South Fork
+              </Dropdown.Item>
+              <Dropdown.Item
+                key='henrys_fork'
+                showFullDescription
+                description='At six thousand feet above sea-level, Henry’s Fork flows out of a spring created by volcanic events more than a millennium ago.'>
+                Henry's Fork
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+          <Navbar.Link isActive href='#'>
+            <Text css={{ fontWeight: 700, color: '#506A63' }}>
+              MEET OUR GUIDES
+            </Text>
           </Navbar.Link>
         </Navbar.Content>
-        <Navbar.Content
-          // Positioning of the navbar items.
-          css={{
-            '@xs': {
-              w: '12%',
-              jc: 'flex-end',
-            },
-          }}></Navbar.Content>
+        <Navbar.Content>
+          <Navbar.Link hideIn={'sm'}>
+            <SlPhone style={{ paddingRight: '5px', color: '#506A63' }} />
+            <Text css={{ fontWeight: 700, color: '#506A63' }}>
+              208-354-2386
+            </Text>
+          </Navbar.Link>
+          <Navbar.Item>
+            <Button
+              auto
+              as={Link}
+              href='#'
+              css={{ backgroundColor: '#F75A0D' }}>
+              <Text css={{ fontWeight: 700, color: '#EAEAEA' }}>BOOK NOW</Text>
+            </Button>
+          </Navbar.Item>
+        </Navbar.Content>
         <Navbar.Collapse>
           {collapseItems.map((item, index) => (
             <Navbar.CollapseItem
               key={item}
               css={{
                 // Hamburger menu color
-                color: 'red',
+                color: '#001C17',
               }}>
               <Link
                 color='inherit'
